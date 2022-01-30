f82ddd6334278c055502fc8d94f394399e3d2d4c
asztonski <asztonski@gmail.com>
2022-01-30 21:14:40 +0000

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import StartBtns, { StartBtnPersonal, StartBtnCharacters, StartBtnItems, StartBtnEsport } from './components/start/start_buttons';
+
+// Ok So basically its a start view that has 4 buttons every button have 25% of window width and 25% of window height
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="box personal" />
+      <button className="box characters" />
+      <button className="box items" />
+      <button className="box esport" />
     </div>
   );
 }
---
+++ src/components/start/start_buttons.js
@@ -0,0 +1,42 @@
+// I still thinking about what should be inside of each buttons...
+
+function StartBtnPersonal() {
+  return (
+    <div className="start_container_personal">
+        <button className="start_button_personal"></button>
+    </div>
+  );
+}
+
+
+function StartBtnCharacters() {
+    return (
+      <div className="start_container_characters">
+          <button className="start_button_characters"></button>
+      </div>
+    );
+}
+
+
+function StartBtnItems() {
+    return (
+      <div className="start_container_items">
+          <button className="start_button_items"></button>
+      </div>
+    );
+}
+
+
+function StartBtnEsport() {
+    return (
+      <div className="start_container_esport">
+          <button className="start_button_esport"></button>
+      </div>
+    );
+  }
+  
+
+
+
+
+export {StartBtnPersonal, StartBtnCharacters, StartBtnItems, StartBtnEsport}
