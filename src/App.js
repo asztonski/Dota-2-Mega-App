71ba82e12e63501b3b714c796cf6f28cdd595d70
Matthew McGrath <rubywep@hotmail.com>
2022-01-30 17:24:30 -0800

    Refactor App to use routes and newly-created Screen components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomeScreen, StatScreen, HeroesScreen, ItemsScreen } from './screens';
 import './App.css';
-import StartBtns, { StartBtnPersonal, StartBtnCharacters, StartBtnItems, StartBtnEsport } from './components/start/start_buttons';
-
-// Ok So basically its a start view that has 4 buttons every button have 25% of window width and 25% of window height
 
 function App() {
   return (
-    <div className='App'>
-      <StartBtnPersonal/>
-      <StartBtnCharacters/>
-      <StartBtnItems/>
-      <StartBtnEsport/>
-    </div>
+    <Router>
+      {/* TODO: Navbar here? */}
+      <Routes>
+        <Route path='/players' element={<StatScreen />} />
+        <Route path='/heroes' element={<HeroesScreen />} />
+        <Route path='/items' element={<ItemsScreen />} />
+        <Route path='/' element={<HomeScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
