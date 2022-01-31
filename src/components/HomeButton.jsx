42ade2a4c6f40bcfc4ae7bd942a29bda986ded17
Matthew McGrath <rubywep@hotmail.com>
2022-01-30 17:18:40 -0800

    Create HomeButton component with temporary logic & styling.

---
+++ src/components/HomeButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './HomeButton.module.css';
+
+function HomeButton({ to, children }) {
+  return (
+    <Link className={styles.HomeButton} to={to}>
+      {children}
+    </Link>
+  );
+}
+
+export default HomeButton;
