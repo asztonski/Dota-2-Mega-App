64b33c29a602f586b53653a8e2ec1d74c4e6d224
Matthew McGrath <rubywep@hotmail.com>
2022-01-30 17:21:44 -0800

    Create HomeScreen with temporary logic & styling. Displays our 4 custom HomeButtons and routes to appropriate URLs.

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import HomeButton from '../components/HomeButton';
+import styles from './HomeScreen.module.css';
+
+// Ok So basically its a start view that has 4 buttons every button have 25% of window width and 25% of window height
+function HomeScreen() {
+  return (
+    <div className={styles.HomeScreen}>
+      {/* <h1 className={styles.Title}>DOTA 2 Mega App</h1> */}
+      <div className={styles.ButtonContainer}>
+        <HomeButton to='/players' className={styles.Button}>
+          Players
+        </HomeButton>
+        <HomeButton to='/heroes' className={styles.Button}>
+          Heroes
+        </HomeButton>
+        <HomeButton to='/items' className={styles.Button}>
+          Items
+        </HomeButton>
+        <HomeButton to='/esports' className={styles.Button}>
+          eSports
+        </HomeButton>
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
