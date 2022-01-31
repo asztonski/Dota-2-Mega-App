2905da8cabd2d91b6ded61adc959ddb6078d0a85
Matthew McGrath <rubywep@hotmail.com>
2022-01-30 17:25:25 -0800

    Create an index file to centralize our Screen imports.

---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import HomeScreen from './HomeScreen';
+import StatScreen from './StatScreen';
+import HeroesScreen from './HeroesScreen';
+import ItemsScreen from './ItemsScreen';
+
+export { HomeScreen, StatScreen, HeroesScreen, ItemsScreen };
