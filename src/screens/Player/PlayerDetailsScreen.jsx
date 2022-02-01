aa7acaa23fc15a9e31144b68b771f658b638faa6
Matthew McGrath <rubywep@hotmail.com>
2022-01-31 18:38:37 -0800

    Add very minor, temporary output and styling to Player Details screen.

--- src/screens/Player/PlayerDetailsScreen.jsx
+++ src/screens/Player/PlayerDetailsScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as playerAPI from '../../apis/playerAPI';
+import styles from './PlayerDetailsScreen.module.css';
 
 // A Screen to show an individual Player's details
 // This will be found at the route: /players/:playerId
@@ -18,13 +19,25 @@ function PlayerDetailsScreen(props) {
 
   if (!player) return 'Loading Player Details';
 
+  // TODO: "Overview" section (or even component?): Renders: Render win-loss numbers, winrate %, last match (hours ago), Rank (as an icon?)
+  // TODO: "Most Played Heroes (All Time)": Renders top 10 heroes by # of matches. Shows hero name, last time ago on that hero, Win %, KDA, Role, Lane
+  // TODO: "Latest Matches": Renders last 10 matches. Shows hero played, skill level, result (win/lose), Type, Duration, and KDA
   return (
-    <div>
-      <div>Account ID: {playerId}</div>
-      <div>MMR Estimate: {player['mmr_estimate'].estimate}</div>
-      <img src={player.profile.avatar} alt={`Avatar for ${player.profile.personaname}`} />
-      <div>Player Name: {player.profile.personaname}</div>
-      <div>Profile URL: {player.profile.profileurl}</div>
+    <div className={styles.PlayerDetails}>
+      <div className={styles.PlayerOverview}>
+        <div className={styles.PlayerHeader}>
+          <img
+            className={styles.ProfileImage}
+            src={player.profile.avatarfull}
+            alt={`Avatar for ${player.profile.personaname}`}
+          />
+          <div className={styles.PlayerName}>{player.profile.personaname}</div>
+        </div>
+        <div>MMR: ~{player['mmr_estimate'].estimate}</div>
+        <div>
+          <a href={player.profile.profileurl}>Steam</a>
+        </div>
+      </div>
     </div>
   );
 }
