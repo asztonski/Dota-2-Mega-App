333158e3b3025ba3b12d5879703d18adf1d24482
Matthew McGrath <rubywep@hotmail.com>
2022-02-01 23:31:16 -0800

    Fetch more player data (recent matches, totals, win/loss) and render it with plain, temporary styling.

--- src/screens/Player/PlayerDetailsScreen.jsx
+++ src/screens/Player/PlayerDetailsScreen.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import * as playerAPI from '../../apis/playerAPI';
-import styles from './PlayerDetailsScreen.module.css';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import * as playerAPI from "../../apis/playerAPI";
+import styles from "./PlayerDetailsScreen.module.css";
 
 // A Screen to show an individual Player's details
 // This will be found at the route: /players/:playerId
@@ -10,14 +10,56 @@ function PlayerDetailsScreen(props) {
 
   const [player, setPlayer] = useState();
 
+  const fetchPlayerStats = async () => {
+    // TODO: Run these 4 (and counting) API calls at once, rather than waiting for one to finish before moving onto the next
+    const basePlayer = await playerAPI.getPlayer(playerId);
+    const playerTotals = await playerAPI.getPlayerTotals(playerId);
+    // The above is an oddly-shaped object, so we need to transform it
+    const playerTotalsTransformed = {};
+
+    for (let i = 0; i < playerTotals.length; i++) {
+      playerTotalsTransformed[playerTotals[i].field] = playerTotals[i].sum;
+    }
+
+    const playerWinLoss = await playerAPI.getPlayerWinLoss(playerId);
+
+    const playerMatches = await playerAPI.getPlayerRecentMatches(playerId);
+
+    // Combine all the fetched data into one all-encomposes object!
+    setPlayer((prevPlayer) => ({
+      ...basePlayer,
+      totals: { ...playerTotalsTransformed },
+      ...playerWinLoss,
+      recentMatches: [...playerMatches],
+    }));
+  };
+
   useEffect(() => {
-    playerAPI.getPlayer(playerId).then((value) => {
-      setPlayer(value);
-      console.log(value);
-    });
+    fetchPlayerStats();
+    console.log("Player", player);
   }, [playerId]);
 
-  if (!player) return 'Loading Player Details';
+  if (!player) return "Loading Player Details";
+
+  const renderedMatches = (
+    <div className={styles.PlayerMatches}>
+      <div className={styles.Header}>Recent Matches</div>
+      <div className={styles.MatchList}>
+        {player.recentMatches.map((match) => {
+          return (
+            <div key={match["match_id"]} className={styles.PlayerMatch}>
+              <div>Duration: {match.duration}</div>
+              <div>Mode: {match["game_mode"]}</div>
+              <div>
+                Kills/Deaths/Assists: {match.kills}/{match.deaths}/
+                {match.assists}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 
   // TODO: "Overview" section (or even component?): Renders: Render win-loss numbers, winrate %, last match (hours ago), Rank (as an icon?)
   // TODO: "Most Played Heroes (All Time)": Renders top 10 heroes by # of matches. Shows hero name, last time ago on that hero, Win %, KDA, Role, Lane
@@ -31,13 +73,27 @@ function PlayerDetailsScreen(props) {
             src={player.profile.avatarfull}
             alt={`Avatar for ${player.profile.personaname}`}
           />
+
           <div className={styles.PlayerName}>{player.profile.personaname}</div>
-        </div>
-        <div>MMR: ~{player['mmr_estimate'].estimate}</div>
-        <div>
-          <a href={player.profile.profileurl}>Steam</a>
+          <div>
+            <a href={player.profile.profileurl}>Steam</a>
+          </div>
+          <div>MMR: ~{player["mmr_estimate"].estimate}</div>
         </div>
       </div>
+
+      <div className={styles.PlayerTotals}>
+        <div className={styles.Header}>Player Totals</div>
+        <div>Kills: {player.totals.kills}</div>
+        <div>Deaths: {player.totals.deaths}</div>
+        <div>Assists: {player.totals.assists}</div>
+      </div>
+      <div className={styles.PlayerWinLoss}>
+        <div className={styles.Header}>Player Win/Loss</div>
+        <div>Win: {player.win}</div>
+        <div>Loss: {player.lose}</div>
+      </div>
+      {renderedMatches}
     </div>
   );
 }
