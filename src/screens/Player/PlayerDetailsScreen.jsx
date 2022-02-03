06b4408be35d65c0a5953c03b5cd8e562b133b09
Matthew McGrath <rubywep@hotmail.com>
2022-02-02 23:34:36 -0800

    Fetch player's heroes data and render some of it. Tweak various rendered elements, along with styling them more closely to the DOTA 2 reference site.

--- src/screens/Player/PlayerDetailsScreen.jsx
+++ src/screens/Player/PlayerDetailsScreen.jsx
@@ -22,8 +22,8 @@ function PlayerDetailsScreen(props) {
     }
 
     const playerWinLoss = await playerAPI.getPlayerWinLoss(playerId);
-
     const playerMatches = await playerAPI.getPlayerRecentMatches(playerId);
+    const playerHeroes = await playerAPI.getPlayerHeroes(playerId);
 
     // Combine all the fetched data into one all-encomposes object!
     setPlayer((prevPlayer) => ({
@@ -31,6 +31,7 @@ function PlayerDetailsScreen(props) {
       totals: { ...playerTotalsTransformed },
       ...playerWinLoss,
       recentMatches: [...playerMatches],
+      heroes: [...playerHeroes],
     }));
   };
 
@@ -41,18 +42,44 @@ function PlayerDetailsScreen(props) {
 
   if (!player) return "Loading Player Details";
 
+  const renderedHeroes = (
+    <div className={styles.PlayerMatches}>
+      <div className={styles.Header}>Most Played Heroes</div>
+      <div className={styles.MatchList}>
+        {player.heroes.slice(0, 10).map((hero) => {
+          return (
+            // TODO: Hero (name, last played), Matches, Win %, KDA
+            // hero_id, last_played, games, win, with_games, with_win, against_games, against_win
+            <div key={hero["match_id"]} className={styles.PlayerMatch}>
+              <div>
+                Hero: {hero["hero_id"]}{" "}
+                <span>Last Played: {hero["last_played"]}</span>
+              </div>
+              <div>Matches: ??</div>
+              <div>Win %: {hero.win}</div>
+              <div>KDA: ??/??/??</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+
   const renderedMatches = (
     <div className={styles.PlayerMatches}>
-      <div className={styles.Header}>Recent Matches</div>
+      <div className={styles.Header}>Latest Matches</div>
       <div className={styles.MatchList}>
         {player.recentMatches.map((match) => {
           return (
+            // TODO: Hero, Result, Type, Duration, KDA
             <div key={match["match_id"]} className={styles.PlayerMatch}>
-              <div>Duration: {match.duration}</div>
+              <div>Hero: {match["hero_id"]}</div>
+              {/* TODO: match["radiant_win"] shows if Radiant won the match...but how do we see if playe was on Radiant team? */}
+              <div>Result: {match["radiant_win"]}</div>
               <div>Mode: {match["game_mode"]}</div>
+              <div>Duration: {(match.duration / 60).toFixed(2)}</div>
               <div>
-                Kills/Deaths/Assists: {match.kills}/{match.deaths}/
-                {match.assists}
+                KDA: {match.kills}/{match.deaths}/{match.assists}
               </div>
             </div>
           );
@@ -67,32 +94,61 @@ function PlayerDetailsScreen(props) {
   return (
     <div className={styles.PlayerDetails}>
       <div className={styles.PlayerOverview}>
-        <div className={styles.PlayerHeader}>
-          <img
-            className={styles.ProfileImage}
-            src={player.profile.avatarfull}
-            alt={`Avatar for ${player.profile.personaname}`}
-          />
-
-          <div className={styles.PlayerName}>{player.profile.personaname}</div>
-          <div>
-            <a href={player.profile.profileurl}>Steam</a>
+        <div className={styles.OverviewProfile}>
+          <div className={styles.ProfileImageContainer}>
+            <img
+              className={styles.ProfileImage}
+              src={player.profile.avatarfull}
+              alt={`Avatar for ${player.profile.personaname}`}
+            />
           </div>
-          <div>MMR: ~{player["mmr_estimate"].estimate}</div>
+          <div className={styles.ProfileContainer}>
+            <div className={styles.PlayerName}>
+              {player.profile.personaname}
+            </div>
+            <div className={styles.ProfileHeader}>Overview</div>
+            <div>
+              <a href={player.profile.profileurl}>Steam</a>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.OverviewWinLoss}>
+          <div className={styles.WinLossContainer}>
+            <span className={styles.PlayerWins}>{player.win}</span>-
+            <span className={styles.PlayerLosses}>{player.lose}</span>
+            <div className={styles.WinLossHeader}>Record</div>
+          </div>
+        </div>
+
+        <div className={styles.OverviewWinrate}>
+          <div className={styles.WinrateContainer}>
+            <div className={styles.Winrate}>
+              {(player.win / (player.win + player.lose)).toFixed(2)}%
+            </div>
+            <div className={styles.WinrateHeader}>Winrate</div>
+          </div>
+        </div>
+
+        <div className={styles.OverviewRanking}>
+          <div className={styles.RankingContainer}>
+            <div>~{player["mmr_estimate"].estimate}</div>
+            <div className={styles.RankingHeader}>MMR</div>
+          </div>
         </div>
       </div>
 
       <div className={styles.PlayerTotals}>
         <div className={styles.Header}>Player Totals</div>
-        <div>Kills: {player.totals.kills}</div>
-        <div>Deaths: {player.totals.deaths}</div>
-        <div>Assists: {player.totals.assists}</div>
+        <div className={styles.TotalsContainer}>
+          <div>Kills: {player.totals.kills}</div>
+          <div>Deaths: {player.totals.deaths}</div>
+          <div>Assists: {player.totals.assists}</div>
+        </div>
       </div>
-      <div className={styles.PlayerWinLoss}>
-        <div className={styles.Header}>Player Win/Loss</div>
-        <div>Win: {player.win}</div>
-        <div>Loss: {player.lose}</div>
-      </div>
+
+      {renderedHeroes}
+
       {renderedMatches}
     </div>
   );
