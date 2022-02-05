500b3de86ea7ab72446ae52ae660278ded308207
Matthew McGrath <rubywep@hotmail.com>
2022-02-05 09:42:55 -0800

    Add green hero text color to Constants.

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -4,3 +4,4 @@ export const backgroundPrimary = '#1c242d';
 export const headerPrimary = '#2e3740'; // Or is it #242f39 ??
 export const heroOdd = '#353f49';
 export const heroEven = '#2d3741';
+export const heroText = '#a9cf54';
