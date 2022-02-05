3b746fa142d7ac442baf635995dcd52fe5cc9726
Matthew McGrath <rubywep@hotmail.com>
2022-02-04 20:00:10 -0800

    Create PlayerOverview component & stylesheet.
    PlayerOverview is a component factored out of PlayerDetailsScreen. It renders the 'Overview' information of the player, such as name, win & loss count, winrate, and rating.

---
+++ src/components/PlayerOverview.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styles from './PlayerOverview.module.css';
+
+function PlayerOverview({ player }) {
+  return (
+    <div className={styles.PlayerOverview}>
+      <div className={styles.OverviewProfile}>
+        <div className={styles.ProfileImageContainer}>
+          <img
+            className={styles.ProfileImage}
+            src={player.profile.avatarfull}
+            alt={`Avatar for ${player.profile.personaname}`}
+          />
+        </div>
+        <div className={styles.ProfileContainer}>
+          <div className={styles.PlayerName}>{player.profile.personaname}</div>
+          <div className={styles.ProfileHeader}>Overview</div>
+          <div>
+            <a href={player.profile.profileurl}>Steam</a>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.OverviewWinLoss}>
+        <div className={styles.WinLossContainer}>
+          <span className={styles.PlayerWins}>
+            {player.win.toLocaleString()}
+          </span>
+          -
+          <span className={styles.PlayerLosses}>
+            {player.lose.toLocaleString()}
+          </span>
+          <div className={styles.WinLossHeader}>Record</div>
+        </div>
+      </div>
+
+      <div className={styles.OverviewWinrate}>
+        <div className={styles.WinrateContainer}>
+          <div
+            className={styles.Winrate}
+            style={{
+              color:
+                player.win / (player.win + player.lose) > 0.5 ? 'green' : 'red',
+            }}
+          >
+            {((player.win / (player.win + player.lose)) * 100).toFixed(2)}%
+          </div>
+          <div className={styles.WinrateHeader}>Winrate</div>
+        </div>
+      </div>
+
+      <div className={styles.OverviewRanking}>
+        <div className={styles.RankingContainer}>
+          <div>~{player['mmr_estimate'].estimate}</div>
+          <div className={styles.RankingHeader}>MMR</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PlayerOverview;
