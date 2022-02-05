d0894131b87241359bab9e915c2d57039d85d219
Matthew McGrath <rubywep@hotmail.com>
2022-02-04 19:40:31 -0800

    Create a helpers folder & file w/ timeHelper func
    This folder will house helper functions. The timeHelper function accepts an amount of seconds as input and returns a user-friendly hours:minutes:seconds string.

---
+++ src/helpers/helpers.js
@@ -0,0 +1,18 @@
+// Converts an amount of seconds into an hour:minute:seconds formatted string
+const timeHelper = (timeInSeconds) => {
+  const dateObj = new Date(timeInSeconds * 1000);
+  const hours = dateObj.getUTCHours();
+  const minutes = dateObj.getUTCMinutes();
+  const seconds = dateObj.getSeconds();
+
+  const timeString =
+    hours.toString().padStart(2, '0') +
+    ':' +
+    minutes.toString().padStart(2, '0') +
+    ':' +
+    seconds.toString().padStart(2, '0');
+
+  return timeString;
+};
+
+export { timeHelper };
