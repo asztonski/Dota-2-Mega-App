ea5eb8eac30c7a1e735965b86a8034fdb4f62225
asztonski <asztonski@gmail.com>
2022-02-06 23:10:28 +0000

    Color variables js file delated, color variables added to app.scss, app.scss has some general styles, style changes for home screen in process

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './HomeButton.module.scss';
+import styles from '../screens/Home/HomeScreen.module.css';
 
 function HomeButton({ to, children }) {
   return (
--- src/constants/colors.js
+++
@@ -1,7 +0,0 @@
-// Temporary way of storing commonly-used Colors for the site
-// For now we never actually access these -- simply using this as a reference sheet
-export const backgroundPrimary = '#1c242d';
-export const headerPrimary = '#2e3740'; // Or is it #242f39 ??
-export const heroOdd = '#353f49';
-export const heroEven = '#2d3741';
-export const heroText = '#a9cf54';
